84d9a915766893f26b6a10e8841a6cc929d5c28d
SebastianMP21 <sebastian.marquez@tecsup.edu.pe>
2022-12-04 22:09:19 -0500

    crud-xamarin

---
+++ Database2022/Database2022/Services/PersonService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Database2022
+{
+    public class PersonService
+    {
+        private readonly AppDbContext _context;
+
+        public PersonService() => _context = App.GetContext();
+
+        
+        public bool Create(Person item)
+        {
+            try
+            {
+                //EntityFrameworkCore
+                _context.People.Add(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+
+
+        public bool CreateRange(List<Person> items)
+        {
+            try
+            {
+                //EntityFrameworkCore
+                _context.People.AddRange(items);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public List<Person> Get()
+        {
+            return _context.People.ToList();
+        }
+
+
+        public List<Person> GetByText(string text)
+        {
+            return _context.People.Where(x => x.FirstName.Contains(text) || x.LastName.Contains(text)).ToList();
+        }
+
+        public Person GetById(int id)
+        {
+            return _context.People.Where(x => x.PersonId == id).FirstOrDefault();
+        }
+
+        public bool UpdatePerson(Person person, int id)
+        {
+            try
+            {
+                var model = GetById(id);
+                model.LastName = person.LastName;
+                model.FirstName = person.FirstName;
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool DeletePerson(int id)
+        {
+            try
+            {
+                var model = GetById(id);
+                _context.Remove(model);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+
+        }
+    }
+}
---
+++ Database2022/Database2022/ViewModels/ViewModelPeople.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Database2022.ViewModels
+{
+    public class ViewModelPeople: BaseViewModel
+    {
+        private string color;
+        public string Color
+        {
+            get { return this.color; }
+            set { SetValue(ref this.color, value); }
+        }
+
+
+        private string filter;
+        public string Filter
+        {
+            get { return this.filter; }
+            set { SetValue(ref this.filter, value); }
+        }
+
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { SetValue(ref this.firstName, value); }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { SetValue(ref this.lastName, value); }
+        }
+
+        private int idPerson;
+        public int IdPerson
+        {
+            get { return idPerson; }
+            set { SetValue(ref this.idPerson, value); }
+        }
+
+        private List<Person> people;
+        public List<Person> People
+        {
+            get { return this.people; }
+            set { SetValue(ref this.people, value); }
+        }
+
+        private Person person;
+        public Person Person
+        {
+            get { return this.person; }
+            set { SetValue(ref this.person, value); }
+        }
+
+        public ICommand SearchCommand { get; set; }
+        public ICommand InsertCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand SelectOneCommand { get; set; }
+        public ViewModelPeople()
+        {
+            PersonService service = new PersonService();
+            People = service.Get();
+
+            SearchCommand = new Command(() =>
+            {
+                PersonService service = new PersonService();
+                People = service.Get();
+            });
+
+            InsertCommand = new Command(() =>
+            {
+                PersonService service = new PersonService();
+                if (IdPerson != 0)
+                {
+                    Console.WriteLine(IdPerson);
+                    service.UpdatePerson(new Person { FirstName=FirstName, LastName = LastName, PersonId=IdPerson}, IdPerson);
+
+                    
+                    IdPerson = 0;
+                }
+                else
+                {
+                    int newPersonId = service.Get().Count + 1;
+                    service.Create(new Person { FirstName = FirstName, LastName = LastName, PersonId = newPersonId });
+                    
+                }
+                People = service.Get();
+            });
+
+            SelectOneCommand = new Command<int>(execute: (int parameter) =>
+            {
+                PersonService service = new PersonService();
+                int ide = parameter;
+                Person = service.GetById(ide);
+                FirstName = Person.FirstName;
+                LastName = Person.LastName;
+                IdPerson = Person.PersonId;
+                //Console.WriteLine(IdPerson);
+            });
+
+            DeleteCommand = new Command<int>(execute: (int parameter) =>
+            {
+                PersonService service = new PersonService();
+                int ide = parameter;
+                service.DeletePerson(ide);
+                People = service.Get();
+            });
+        }
+
+
+    }
+}
